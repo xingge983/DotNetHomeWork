ca1b6d16533fc2e4f236c7a3a0073516675d545d
xingge983 <44830513+xingge983@users.noreply.github.com>
2020-10-21 17:18:44 +0800

    Add files via upload

---
+++ Homework1/埃氏筛法/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            for(int i = 0; i < 100; i++)
+            {
+                a[i] = 1;
+            }
+            for(int i = 2; i < 100; i++)
+            {
+                for(int j = 2; i*j < 100; j++)
+                {
+                    a[i * j] = 0;
+                }
+            }
+
+            for(int i = 2; i < 100; i++)
+            {
+                if (a[i] == 1)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
